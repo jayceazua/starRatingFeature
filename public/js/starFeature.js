70d10e4f466924e7b682988c39cee4cc304f7ee8
Jayce Azua <jayce.azua@gmail.com>
2018-05-23 20:54:39 -0400

    ehh

--- public/js/starFeature.js
+++ public/js/starFeature.js
@@ -58,10 +58,12 @@ StarRating.prototype.redirectClickOnStars = function(e) {
 
   if (starValue <= 2) {
     console.log('send an email.');
+    //<a class="" title="" href="mailto:bayley@digitalfuturemarketing.com?subject=How%20Can%20We%20Do%20Better%3F&amp;body=Tell%20us%20about%20your%20experience%2C%20and%20we%27ll%20do%20our%20best%20to%20make%20it%20right.%20" target="_blank">
   } else {
     console.log('redirect to leave a review');
+    // <a href="https://www.yelp.com/writeareview/biz/eQ92EcJlPPN_IZV9nOiA_g?return_url=%2Fbiz%2FeQ92EcJlPPN_IZV9nOiA_g&amp;source=biz_details_war_button" target="_blank">
   }
-  
+
 };
 
 // Run:
